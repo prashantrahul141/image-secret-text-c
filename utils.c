64188943c65de2896807de47812802f78bf39ed6
Prashant <prashantrahul141@protonmail.com>
2023-10-13 01:36:26 +0530

    add : print buffer slice and static help message.

--- utils.c
+++ utils.c
@@ -4,6 +4,16 @@
 #include <unistd.h>
 #include <stdint.h>
 
+char *get_help_message()
+{
+  static char *help_message = "Storing Text in PNG Images using C. \
+\nFile format supported : PNG.\n\
+Arguments:\n\
+\t - k : Method to use.\
+\n\t - f : filename.\n";
+  return help_message;
+}
+
 void read_buffer_from_file(FILE *fileptr, uint8_t *buffer, size_t buffer_size)
 {
   size_t n = fread(buffer, buffer_size, 1, fileptr);
@@ -24,5 +34,13 @@ void read_buffer_from_file(FILE *fileptr, uint8_t *buffer, size_t buffer_size)
       printf("[ERROR] : Some unknown error occured while reading buffer.\n");
       exit(1);
     }
+  }
+}
+
+void print_buffer_slice(uint8_t *buffer, size_t size)
+{
+  for (size_t i = 0; i < size; i++)
+  {
+    printf("%u ", buffer[i]);
   }
 }
--- utils.h
+++ utils.h
@@ -5,3 +5,6 @@
 #include <stdint.h>
 
 void read_buffer_from_file(FILE *fileptr, uint8_t *buffer, size_t buffer_size);
+
+void print_buffer_slice(uint8_t *buffer, size_t size);
+char *get_help_message();
