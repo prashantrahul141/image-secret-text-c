e0b87b1f0728ca1c4d3235fd4321e3f99e7b4141
Prashant <prashantrahul141@protonmail.com>
2023-10-13 01:24:47 +0530

    add utils header file

---
+++ utils.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdint.h>
+
+void read_buffer_from_file(FILE *fileptr, uint8_t *buffer, size_t buffer_size);
