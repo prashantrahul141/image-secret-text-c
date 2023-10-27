7481f5d894e1cbf7d3966b030fd9b3541f402ce5
Prashant <prashantrahul141@protonmail.com>
2023-10-27 19:56:28 +0530

    proper format specifiers

--- main.c
+++ main.c
@@ -3,25 +3,23 @@
    The PNG Specification : https://www.rfc-editor.org/rfc/rfc2083
 */
 
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include <unistd.h>
-#include <string.h>
-#include <stdio.h>
-#include <stdint.h>
 
 // to enable debug mode.
-// #define DEBUG
+#define DEBUG
 
 // DEBUG print macro.
 #ifdef DEBUG
 #define DEBUG_PRINT(x) printf x
 #else
-#define DEBUG_PRINT(x) \
-  do                   \
-  {                    \
+#define DEBUG_PRINT(x)                                                         \
+  do {                                                                         \
   } while (0)
 #endif
 
@@ -31,14 +29,14 @@
 char *SECRET_CHUNK_TYPE = "saNS";
 
 #define PNG_SIGNATURE_SIZE 8
-const uint8_t PNG_SIGNATURE[PNG_SIGNATURE_SIZE] = {137, 80, 78, 71, 13, 10, 26, 10};
+const uint8_t PNG_SIGNATURE[PNG_SIGNATURE_SIZE] = {137, 80, 78, 71,
+                                                   13,  10, 26, 10};
 
 #define IDAT_CHUNK_COPY_SIZE (32 * 1024)
 uint32_t idat_chunk_copy_data[IDAT_CHUNK_COPY_SIZE];
 
 /// @brief returns help message string.
-char *get_help_message()
-{
+void get_help_message() {
   static char *help_message = "Storing Text in PNG Images using C. \
 \nFile format supported : PNG.\n\
 Arguments:\n\
@@ -47,31 +45,24 @@ Arguments:\n\
 \t - d : Decode text in the given file.\n\
 \t - t : Text which will be encoded in the image. takes <text> as paramters.";
 
-  return help_message;
+  printf("%s", help_message);
 }
 
 /// @brief read buffer from a file.
 /// @param fileptr
 /// @param buffer
 /// @param buffer_size
-void read_buffer_from_file(FILE *fileptr, void *buffer, size_t buffer_size)
-{
-  DEBUG_PRINT(("[READ_BUFFER_FROM_FILE] : %d bytes\n", buffer_size));
+void read_buffer_from_file(FILE *fileptr, void *buffer, size_t buffer_size) {
+  DEBUG_PRINT(("[READ_BUFFER_FROM_FILE] : %zu bytes\n", buffer_size));
   size_t n = fread(buffer, buffer_size, 1, fileptr);
-  if (n != 1)
-  {
-    if (ferror(fileptr))
-    {
+  if (n != 1) {
+    if (ferror(fileptr)) {
       fprintf(stderr, "[ERROR] : Could not read buffer.\n");
       exit(1);
-    }
-    else if (feof(fileptr))
-    {
+    } else if (feof(fileptr)) {
       fprintf(stderr, "[ERROR] : could not read buffer, reached EOF.\n");
       exit(1);
-    }
-    else
-    {
+    } else {
       printf("[ERROR] : Some unknown error occured while reading buffer.\n");
       exit(1);
     }
@@ -82,24 +73,17 @@ void read_buffer_from_file(FILE *fileptr, void *buffer, size_t buffer_size)
 /// @param fileptr
 /// @param buffer
 /// @param buffer_size
-void write_buffer_to_file(FILE *fileptr, void *buffer, size_t buffer_size)
-{
-  DEBUG_PRINT(("[WRITE_BUFFER_FROM_FILE] : %d bytes\n", buffer_size));
+void write_buffer_to_file(FILE *fileptr, void *buffer, size_t buffer_size) {
+  DEBUG_PRINT(("[WRITE_BUFFER_FROM_FILE] : %zu bytes\n", buffer_size));
   size_t n = fwrite(buffer, buffer_size, 1, fileptr);
-  if (n != 1)
-  {
-    if (ferror(fileptr))
-    {
+  if (n != 1) {
+    if (ferror(fileptr)) {
       fprintf(stderr, "[ERROR] : Could not read buffer.\n");
       exit(1);
-    }
-    else if (feof(fileptr))
-    {
+    } else if (feof(fileptr)) {
       fprintf(stderr, "[ERROR] : could not write buffer, reached EOF.\n");
       exit(1);
-    }
-    else
-    {
+    } else {
       printf("[ERROR] : Some unknown error occured while writing buffer.\n");
       exit(1);
     }
@@ -109,10 +93,8 @@ void write_buffer_to_file(FILE *fileptr, void *buffer, size_t buffer_size)
 /// @brief prints a buffer within a size limit.
 /// @param buffer
 /// @param size
-void print_buffer_slice(char *buffer, size_t size_cap)
-{
-  for (size_t i = 0; i < size_cap; i++)
-  {
+void print_buffer_slice(char *buffer, size_t size_cap) {
+  for (size_t i = 0; i < size_cap; i++) {
     printf("%c", buffer[i]);
   }
 }
@@ -120,11 +102,9 @@ void print_buffer_slice(char *buffer, size_t size_cap)
 /// @brief Reverse bytes in a memory buffer.
 /// @param buffer
 /// @param size
-void reverse_bytes_order(void *buffer_, size_t size_cap)
-{
+void reverse_bytes_order(void *buffer_, size_t size_cap) {
   uint8_t *buffer = buffer_;
-  for (size_t i = 0; i < size_cap / 2; i++)
-  {
+  for (size_t i = 0; i < size_cap / 2; i++) {
     uint8_t t = buffer[i];
     buffer[i] = buffer[size_cap - i - 1];
     buffer[size_cap - i - 1] = t;
@@ -133,20 +113,17 @@ void reverse_bytes_order(void *buffer_, size_t size_cap)
 
 /// @brief Prints decoded result.
 /// @param buffer
-void print_decoded_result(char *buffer, size_t buffer_size)
-{
+void print_decoded_result(char *buffer, size_t buffer_size) {
   printf("DECODED TEXT : ");
   print_buffer_slice(buffer, buffer_size);
   printf("\n\n");
 }
 
 // ENTRY POINT.
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 
-  if (argc <= 1)
-  {
-    printf(get_help_message());
+  if (argc <= 1) {
+    get_help_message();
     exit(0);
   }
 
@@ -159,12 +136,10 @@ int main(int argc, char *argv[])
   char *filename = NULL;
   char *encoding_data = NULL;
   bool DECODING_MODE = true;
-  while ((option = getopt(argc, argv, "het:df:")) != -1)
-  {
-    switch (option)
-    {
+  while ((option = getopt(argc, argv, "het:df:")) != -1) {
+    switch (option) {
     case 'h':
-      printf(get_help_message());
+      get_help_message();
       exit(0);
 
     case 'e':
@@ -173,10 +148,10 @@ int main(int argc, char *argv[])
       break;
 
     case 't':
-      if (!DECODING_MODE)
-      {
+      if (!DECODING_MODE) {
         encoding_data = optarg;
-        printf("DATA TO BE ENCODED IN THE IMAGE : %s (%d bytes).\n", encoding_data, strlen(encoding_data));
+        printf("DATA TO BE ENCODED IN THE IMAGE : %s (%li bytes).\n",
+               encoding_data, strlen(encoding_data));
       }
       break;
 
@@ -191,7 +166,8 @@ int main(int argc, char *argv[])
       break;
 
     default:
-      fprintf(stderr, "[ERROR] : Could not parse these command line arguments.\n");
+      fprintf(stderr,
+              "[ERROR] : Could not parse these command line arguments.\n");
       exit(1);
       break;
     }
@@ -203,18 +179,15 @@ int main(int argc, char *argv[])
 
   bool found_secret_chunk = false;
 
-  if (!DECODING_MODE)
-  {
+  if (!DECODING_MODE) {
     // in encoding mode.
-    if (strlen(encoding_data) <= 0)
-    {
+    if (strlen(encoding_data) <= 0) {
       fprintf(stderr, "[ERROR] : Encoding text cannot be 0 characters long.\n");
       exit(1);
     }
 
     output_file_ptr = fopen("output.png", "wb");
-    if (!output_file_ptr)
-    {
+    if (!output_file_ptr) {
       fprintf(stderr, "Could not create output file.\n");
       exit(1);
     }
@@ -223,92 +196,87 @@ int main(int argc, char *argv[])
   // trying to read png signature.
   uint8_t signature[PNG_SIGNATURE_SIZE];
   read_buffer_from_file(input_file_ptr, signature, sizeof(signature));
-  if (!DECODING_MODE)
-  {
+  if (!DECODING_MODE) {
     write_buffer_to_file(output_file_ptr, signature, sizeof(signature));
   }
 
-  if (memcmp(signature, PNG_SIGNATURE, 8) != 0)
-  {
+  if (memcmp(signature, PNG_SIGNATURE, 8) != 0) {
     printf("[ERROR] : Given image doesn\'t have the correct file signature.");
     exit(1);
   }
 
   bool read_buffer = true;
-  while (read_buffer)
-  {
+  bool done_encoding = false;
+  while (read_buffer) {
     // reading length.
     uint32_t data_chunk_size;
-    read_buffer_from_file(input_file_ptr, &data_chunk_size, sizeof(data_chunk_size));
+    read_buffer_from_file(input_file_ptr, &data_chunk_size,
+                          sizeof(data_chunk_size));
 
     // chunk type.
     uint8_t chunk_type[4];
     read_buffer_from_file(input_file_ptr, chunk_type, sizeof(chunk_type));
 
-    if (!DECODING_MODE)
-    {
+    if (!DECODING_MODE) {
       // writing output files in encoding mode.
-      write_buffer_to_file(output_file_ptr, &data_chunk_size, sizeof(data_chunk_size));
+      write_buffer_to_file(output_file_ptr, &data_chunk_size,
+                           sizeof(data_chunk_size));
       write_buffer_to_file(output_file_ptr, chunk_type, sizeof(chunk_type));
     }
 
     reverse_bytes_order(&data_chunk_size, sizeof(data_chunk_size));
 
-    if (!DECODING_MODE)
-    {
+    if (!DECODING_MODE) {
       // when in encoding mode.
       float iterations = data_chunk_size / (float)sizeof(idat_chunk_copy_data);
       uint32_t temp_data_chunk_size = data_chunk_size;
-      DEBUG_PRINT(("Iterations required : %f for IDAT_SIZE : %d and DATA_CHUNK_SIZE : %d\n", iterations, sizeof(idat_chunk_copy_data), data_chunk_size));
-      if (iterations > 1.0f)
-      {
+      DEBUG_PRINT(("Iterations required : %f for IDAT_SIZE : %lu and "
+                   "DATA_CHUNK_SIZE : %d\n",
+                   iterations, sizeof(idat_chunk_copy_data), data_chunk_size));
+      if (iterations > 1.0f) {
         // when there are more data than holding capacity.
-        for (;;)
-        {
-          if (temp_data_chunk_size > sizeof(idat_chunk_copy_data))
-          {
-            read_buffer_from_file(input_file_ptr, &idat_chunk_copy_data, sizeof(idat_chunk_copy_data));
-            write_buffer_to_file(output_file_ptr, &idat_chunk_copy_data, sizeof(idat_chunk_copy_data));
-            temp_data_chunk_size = (size_t)(temp_data_chunk_size - (size_t)sizeof(idat_chunk_copy_data));
-          }
-          else
-          {
-            read_buffer_from_file(input_file_ptr, &idat_chunk_copy_data, temp_data_chunk_size);
-            write_buffer_to_file(output_file_ptr, &idat_chunk_copy_data, temp_data_chunk_size);
+        for (;;) {
+          if (temp_data_chunk_size > sizeof(idat_chunk_copy_data)) {
+            read_buffer_from_file(input_file_ptr, &idat_chunk_copy_data,
+                                  sizeof(idat_chunk_copy_data));
+            write_buffer_to_file(output_file_ptr, &idat_chunk_copy_data,
+                                 sizeof(idat_chunk_copy_data));
+            temp_data_chunk_size =
+                (size_t)(temp_data_chunk_size -
+                         (size_t)sizeof(idat_chunk_copy_data));
+          } else {
+            read_buffer_from_file(input_file_ptr, &idat_chunk_copy_data,
+                                  temp_data_chunk_size);
+            write_buffer_to_file(output_file_ptr, &idat_chunk_copy_data,
+                                 temp_data_chunk_size);
             break; // just to be sure.
           }
         }
-      }
-      else
-      {
-        if (data_chunk_size != 0)
-        {
+      } else {
+        if (data_chunk_size != 0) {
           // when there are less data than holding capacity.
           DEBUG_PRINT(("writing entire chunk in once.\n"));
-          read_buffer_from_file(input_file_ptr, &idat_chunk_copy_data, data_chunk_size);
-          write_buffer_to_file(output_file_ptr, &idat_chunk_copy_data, data_chunk_size);
+          read_buffer_from_file(input_file_ptr, &idat_chunk_copy_data,
+                                data_chunk_size);
+          write_buffer_to_file(output_file_ptr, &idat_chunk_copy_data,
+                               data_chunk_size);
         }
       }
-    }
-    else
-    {
+    } else {
       // decoding acutal text.
-      if (*(char *)chunk_type == *SECRET_CHUNK_TYPE)
-      {
+      if (*(char *)chunk_type == *SECRET_CHUNK_TYPE) {
         DEBUG_PRINT(("Chunk found.\n"));
         found_secret_chunk = true;
         char *secret_data_buffer = (char *)malloc(data_chunk_size);
-        read_buffer_from_file(input_file_ptr, secret_data_buffer, data_chunk_size);
+        read_buffer_from_file(input_file_ptr, secret_data_buffer,
+                              data_chunk_size);
         print_decoded_result(secret_data_buffer, data_chunk_size);
         free(secret_data_buffer);
         fclose(input_file_ptr);
         exit(0);
-      }
-      else
-      {
+      } else {
         // when decoding if the chunk type is not saNS we can just skip it.
-        if (fseek(input_file_ptr, data_chunk_size, SEEK_CUR) != 0)
-        {
+        if (fseek(input_file_ptr, data_chunk_size, SEEK_CUR) != 0) {
           fprintf(stderr, "Failed to seek input file data chunk.");
           exit(1);
         }
@@ -319,19 +287,18 @@ int main(int argc, char *argv[])
     read_buffer_from_file(input_file_ptr, &chunk_crc, sizeof(chunk_crc));
 
     // secret chunk stuff.
-    if (!DECODING_MODE)
-    {
+    if (!DECODING_MODE) {
       write_buffer_to_file(output_file_ptr, &chunk_crc, sizeof(chunk_crc));
 
       // writing secret text to output image.
-      if (*(uint32_t *)chunk_type == IDAT_CHUNK_TYPE)
-      {
+      if (*(uint32_t *)chunk_type == IDAT_CHUNK_TYPE && !done_encoding) {
         printf("ENCODING SECRET CHUNK.\n");
 
         // writing  length chunk.
         uint32_t secret_chunk_size = strlen(encoding_data);
         reverse_bytes_order(&secret_chunk_size, sizeof(secret_chunk_size));
-        write_buffer_to_file(output_file_ptr, &secret_chunk_size, sizeof(secret_chunk_size));
+        write_buffer_to_file(output_file_ptr, &secret_chunk_size,
+                             sizeof(secret_chunk_size));
         reverse_bytes_order(&secret_chunk_size, sizeof(secret_chunk_size));
 
         // writing  type chunk.
@@ -342,21 +309,24 @@ int main(int argc, char *argv[])
         write_buffer_to_file(output_file_ptr, encoding_data, secret_chunk_size);
 
         // writing crc.
-        write_buffer_to_file(output_file_ptr, &secret_chunk_crc, sizeof(secret_chunk_crc));
+        write_buffer_to_file(output_file_ptr, &secret_chunk_crc,
+                             sizeof(secret_chunk_crc));
+
+        done_encoding = true;
       }
     }
 
     // printing some info about the current chunk.
     DEBUG_PRINT(("--------------------------------------\n"));
     DEBUG_PRINT(("Chunk Data Size : %u\n", data_chunk_size));
-    DEBUG_PRINT(("Chunk Type : %.*s (0x%08X)\n", (int)sizeof(chunk_type), chunk_type, *(uint32_t *)chunk_type));
+    DEBUG_PRINT(("Chunk Type : %.*s (0x%08X)\n", (int)sizeof(chunk_type),
+                 chunk_type, *(uint32_t *)chunk_type));
     DEBUG_PRINT(("Chunk CRC : 0x%08X\n", chunk_crc));
     DEBUG_PRINT(("--------------------------------------\n"));
 
     // stop reading chunks if hit the IEND chunk which marks
     // the end of chunks according to png specification.
-    if (*(uint32_t *)chunk_type == IEND_CHUNK_TYPE)
-    {
+    if (*(uint32_t *)chunk_type == IEND_CHUNK_TYPE) {
       DEBUG_PRINT(("Reached IEND Chunk.\n"));
       read_buffer = false;
     }
@@ -364,23 +334,20 @@ int main(int argc, char *argv[])
 
   // closes file.
   fclose(input_file_ptr);
-  if (!DECODING_MODE)
-  {
+  if (!DECODING_MODE) {
     printf("ENCODING COMPLETE.");
     fclose(output_file_ptr);
-  }
-  else
-  {
-    if (!found_secret_chunk)
-    {
-      printf("Could\'nt found any secret chunk in the given image, make sure the image you\'re providing does have a secret text created using this program.\n");
+  } else {
+    if (!found_secret_chunk) {
+      printf("Could\'nt found any secret chunk in the given image, make sure "
+             "the image you\'re providing does have a secret text created "
+             "using this program.\n");
     }
   }
 
   // end time.
   endTime = (double)clock() / CLOCKS_PER_SEC;
-  printf("Total duration it took: %lfs\n",
-         endTime - startTime);
+  printf("Total duration it took: %lfs\n", endTime - startTime);
 
   return 0;
 }
